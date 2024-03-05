ea2c6ec998ae878ee99bc5ddf887a99c5d19f9bb
migo <migo@student.42.seoul.kr>
2024-03-05 13:18:01 +0900

    two

---
+++ shell.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get install -y sudo
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y ca-certificates
+sudo apt-get install -y curl
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y git
+sudo apt-get install -y make
+sudo apt-get install -y vim
+sudo apt-get install -y systemd
