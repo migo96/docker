8fd492e9cbec9339daee7713190dee5603b2c546
migo <migo@student.42.seoul.kr>
2024-03-06 14:54:00 +0900

    shell

--- shell.sh
+++ shell.sh
@@ -9,17 +9,7 @@ sudo apt-get install -y make
 sudo apt-get install -y vim
 sudo apt-get install -y systemd
 sudo apt-get update
-
-# 도커 패키지 신뢰성 추가
-# f - http의 요청 헤더의 Content-Type을 multipart/form-data로 설정
-# s - silent로 진행과정 및 오류 생략
-# S - SSL 이용
-# L - curl의 결과가 Redirection이더라도, 해당 URL로 작업을 이어감
-# 파이프 이후의 apt-key는 다운로드 받은 패키지를 인증된 패키지로 인식 시키기 위해 키 리스트에 패키지를 추가
-# (apt는 기본적으로 인증된 패키지들을 키 리스트로 관리)
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-
-# 도커를 설치할 수 있도록 Repository 추가
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker.io
@@ -38,3 +28,6 @@ sudo mkdir -p /mnt/cdrom
 sudo mount /dev/cdrom /mnt/cdrom
 cd /mnt/cdrom
 sudo sh ./VBoxLinuxAdditions.run —nox11
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
